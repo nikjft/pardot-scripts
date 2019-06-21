ee684f9e05099bcf068c9dc434cc503565d1fb03
nikjft <nikjft@users.noreply.github.com>
2019-06-21 13:53:50 -0600

    Create Variable Content.js

---
+++ Variable Content.js
@@ -0,0 +1,13 @@
+// Default Content
+
+<script type="text/javascript">
+	window.dataLayer = window.dataLayer || [];
+	window.dataLayer.push({'event':'pardot-id','pardot-status':'visitor'})
+</script>
+
+// When email contains '@'
+
+<script type="text/javascript">
+	window.dataLayer = window.dataLayer || [];
+	window.dataLayer.push({'event':'pardot-id','pardot-status':'prospect','pardot-email':'%%email%%','pardot-first-name':'%%first_name%%','pardot-last-name':'%%last_name%%','pardot-company':'%%company%%'})
+</script>
